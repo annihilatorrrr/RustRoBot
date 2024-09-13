696b0c0ae9efca5fffa7cef4bc6073c702d46382
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2024-09-13 18:54:57 +0530

    Done.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use async_std::task;
 use ferrisgram::error::{GroupIteration, Result};
 use ferrisgram::ext::filters::message;
 use ferrisgram::ext::handlers::{CommandHandler, MessageHandler};
@@ -5,13 +6,14 @@ use ferrisgram::ext::{Context, Dispatcher, Updater};
 use ferrisgram::types::LinkPreviewOptions;
 use ferrisgram::Bot;
 use std::env;
+use std::time::Duration;
 
 #[allow(unused)]
 #[tokio::main]
 async fn main() {
     let token = match env::var("TOKEN") {
         Ok(s) => s,
-        Err(err) => panic!("failed to get bot token: {}", err),
+        Err(_) => "YOU MAY PUT THE TOKEN HERE - For hardcode lovers.".to_string(),
     };
     let bot = match Bot::new(&token, None).await {
         Ok(bot) => bot,
@@ -19,13 +21,17 @@ async fn main() {
     };
     let mut dispatcher = &mut Dispatcher::new(&bot);
     dispatcher.add_handler(CommandHandler::new("start", start));
+    dispatcher.add_handler(CommandHandler::new("ping", pingh));
+    dispatcher.add_handler(CommandHandler::new("sleep", sysnchk));
     dispatcher.add_handler_to_group(
         MessageHandler::new(echo, message::Text::filter().or(message::Caption::filter())),
         1,
     );
     let mut updater = Updater::new(&bot, dispatcher);
+    updater.allowed_updates = Some(vec!["message"]);
     println!("Started!");
     updater.start_polling(false).await;
+    println!("Bye!");
 }
 
 async fn start(bot: Bot, ctx: Context) -> Result<GroupIteration> {
@@ -34,12 +40,11 @@ async fn start(bot: Bot, ctx: Context) -> Result<GroupIteration> {
     link_preview_options.is_disabled = Some(true);
     msg.reply(
         &bot,
-        "Hey! I am an echo bot built using [Ferrisgram](https://github.com/ferrisgram/ferrisgram).
-I will repeat your messages.",
+        "Hey! I am an echo bot built in love with [Ferrisgram](https://github.com/ferrisgram/ferrisgram).
+I will just repeat your messages.",
     )
     .parse_mode("markdown".to_string())
     .link_preview_options(link_preview_options)
-    // You must use this send() method in order to send the request to the API
     .send()
     .await?;
     Ok(GroupIteration::EndGroups)
@@ -49,6 +54,33 @@ async fn echo(bot: Bot, ctx: Context) -> Result<GroupIteration> {
     let chat = ctx.effective_chat.unwrap();
     let msg = ctx.effective_message.unwrap();
     bot.copy_message(chat.id, chat.id, msg.message_id)
+        .send()
+        .await?;
+    Ok(GroupIteration::EndGroups)
+}
+
+async fn sysnchk(b: Bot, ctx: Context) -> Result<GroupIteration> {
+    let msg = ctx.effective_message.unwrap();
+    let emsg = msg.reply(&b, "Sleeping 10s ...").send().await?;
+    task::sleep(Duration::from_secs(10)).await;
+    b.edit_message_text("Done!".to_string())
+        .chat_id(msg.chat.id)
+        .message_id(emsg.message_id)
+        .send()
+        .await?;
+    Ok(GroupIteration::EndGroups)
+}
+
+async fn pingh(b: Bot, ctx: Context) -> Result<GroupIteration> {
+    let msg = ctx.effective_message.unwrap();
+    task::sleep(Duration::from_secs(10)).await;
+    let startt = std::time::Instant::now();
+    let emsg = msg.reply(&b, "Pinging ...").send().await?;
+    let endt = std::time::Instant::now();
+    let elapsed_time = endt - startt;
+    b.edit_message_text(format!("Pong!\n{:?}!", elapsed_time))
+        .chat_id(msg.chat.id)
+        .message_id(emsg.message_id)
         .send()
         .await?;
     Ok(GroupIteration::EndGroups)
